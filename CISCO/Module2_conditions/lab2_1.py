88414c1068fd28b635860c2802906029b9890344
Serhii Kroshka <pm012@ukr.net>
2025-01-28 20:45:03 +0200

    cisco lab module2

--- CISCO/Module2_conditions/lab2_1.py
+++ CISCO/Module2_conditions/lab2_1.py
@@ -13,8 +13,20 @@ def income_tax():
     print("The tax is: ", tax, " thalers")
 
 def leap_year():
-    '''
-As you surely know, due to some astronomical reasons, years may be leap or common. The former are 366 days long, while the latter are 365 days long.
+    year = int(input("Enter a year: "))
+
+    if year < 1582:
+	    print("Not within the Gregorian calendar period")
+    else:
+        if (year % 4 == 0 and year % 100 != 0) or (year % 400) == 0:
+            print("It's a leap year")
+        else:
+            print("It's a common year")
+
+    #  Write the if-elif-elif-else block here.
+
+
+''' As you surely know, due to some astronomical reasons, years may be leap or common. The former are 366 days long, while the latter are 365 days long.
 
 Since the introduction of the Gregorian calendar (in 1582), the following rule is used to determine the kind of year:
 
@@ -29,9 +41,8 @@ The code should output one of two possible messages, which are Leap year or Comm
 
 It would be good to verify if the entered year falls into the Gregorian era, and output a warning otherwise: Not within the Gregorian calendar period. Tip: use the != and % operators.
 
-Test your code using the data we've provided.
+Test your code using the data we've provided. '''
 
-'''
 
 
 if __name__ == "__main__":
