89a9648af7d009816591051bf5fd5e19bfeae148
Serhii Kroshka <pm012@ukr.net>
2025-04-08 18:13:17 +0300

    asynchronious work with files and directories

--- WEB/async/aiofile_sample.py
+++ WEB/async/aiofile_sample.py
@@ -1,12 +1,28 @@
 import asyncio
-from aiofile import async_open
+from aiofile import async_open # for async file handlin 
+from aiopath import AsyncPath
 
 
 async def main():
+    apath = AsyncPath("hello.txt")
+    print(f"Check if hello.txt present: {await apath.exists()}")
+    print(f"Is a file: {await apath.is_file()}")
+    print(f"Is dir: {await apath.is_dir()}")
     async with async_open("hello.txt", 'w+') as afp:
         await afp.write("Hello ")
         await afp.write("world\n")
         await afp.write("Hello from - async world!")
+        
+    print("##############apf.read() -read all lines########################")
+    async with async_open("hello.txt", 'r') as afp:
+        print(await afp.read())
+        
+    print("##############Line by line (async for)########################")
+    async with async_open("hello.txt", 'r') as afp:        
+        async for line in afp:
+            print(line, end='') # can be line.strip() as well to avoid usless line gap between rows
+            
+    
 
 
 if __name__ == '__main__':
