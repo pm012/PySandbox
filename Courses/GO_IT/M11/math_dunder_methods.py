509f329cf215a85839fe05e4e5c2490abc09ccf8
Serhii Kroshka <pm012@ukr.net>
2025-04-27 22:34:26 +0300

    mathematical operations overriding using magic methods (improved code to avoid key error)

---
+++ Courses/GO_IT/M11/math_dunder_methods.py
@@ -0,0 +1,29 @@
+from collections import UserDict
+
+class MyDict(UserDict):
+    def __add__(self, other):
+        new_dict = MyDict(self.data) # copy of self
+        new_dict.update(other)
+        return new_dict
+
+
+    def __sub__(self, other):
+        new_dict = MyDict(self.data)
+        for key in new_dict:
+            if key in other:
+                new_dict.pop(key, None) # remove key if exists (to avoid key error)
+                return new_dict
+
+
+
+
+d1 = MyDict({1: 'a', 2: 'b'})
+d2 = MyDict({3: 'c', 4: 'd'})
+
+
+d3 = d1 + d2
+print(d3)# {1: 'a', 2: 'b', 3: 'c', 4: 'd'}
+
+
+d4 = d3 - d2
+print(d4)# {1: 'a', 2: 'b'}
